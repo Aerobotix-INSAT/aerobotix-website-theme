4f1a68f5b9edbbc432ae0905d6e10cdf55b2bf4d
mahdi-chaari <mahdi125@outlook.com>
2022-09-10 16:31:57 +0100

    better alignments

--- template-parts/loop/loop.php
+++ template-parts/loop/loop.php
@@ -5,7 +5,7 @@
     else $query=$wp_query;
 ?>
 <div class="flex flex-row justify-center">
-    <div class="m-auto grid gap-9 justify-items-center auto-rows-auto grid-cols-1 md:grid-cols-[repeat(2,20rem)] xl:grid-cols-[repeat(3,20rem)] 2xl:grid-cols-[repeat(4,20rem)]">
+    <div class="m-auto grid gap-9 justify-items-center auto-rows-auto grid-cols-1 md:grid-cols-[repeat(2,fit-content(20rem))] xl:grid-cols-[repeat(3,fit-content(20rem))] 2xl:grid-cols-[repeat(4,fit-content(20rem))]">
         <?php if ($query->have_posts()) :
             while ($query->have_posts()) :
                 $query->the_post();
