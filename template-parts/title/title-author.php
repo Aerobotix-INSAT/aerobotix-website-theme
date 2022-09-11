aa73e6a7619d8676ec9d12c5467d369e6d527699
mahdi-chaari <mahdi125@outlook.com>
2022-09-11 23:56:37 +0100

    added author description

--- template-parts/title/title-author.php
+++ template-parts/title/title-author.php
@@ -6,4 +6,5 @@ $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : g
         <?php echo get_avatar($curauth, 60, "", "", ["class" => "object-cover w-32 h-32"]) ?>
     </div>
     <div class="font-black text-3xl md:text-5xl break-all"><?php echo ucwords($curauth->first_name . ' ' . $curauth->last_name."'s Articles"); ?></div>
+    <p class="text-normal text-sm md:text-base"><?php echo get_the_author_meta('description',$curauth->ID) ?></p>
 </div>
